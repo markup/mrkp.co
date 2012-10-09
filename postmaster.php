f7261ef1496ccdb4f4bfbf303a618a4cac68adcc
Alfonso <alfonso@agarzola.com>
2012-10-09 17:33:45 -0400

    Added email script.

---
+++ postmaster.php
@@ -0,0 +1,24 @@
+<?php
+
+  if (!empty($_POST['Name']) && !empty($_POST['Email']) && !empty($_POST['Message'])) {
+
+    $to = 'alfonso@mrkp.co'; // your email address, can be @gmail.com, etc.
+    $subject = 'Contact from mrkp.co'; // change yoursite.com to your own domain name
+    $message = "Name: ".$_POST['Name']."\r\nEmail: ".$_POST['Email']."\r\nStatus: ".$_POST['Status']."\r\n\r\nMessage:\r\n".$_POST['Message']."\r\n\r\nSender IP: ".$_SERVER["REMOTE_ADDR"];
+    $headers = 'From: '.$_POST['Email']."\r\n".
+                'Reply-To: '.$_POST['Email']."\r\n";
+    mail($to, $subject, $message, $headers);
+
+    header("Status: 200 OK", true, 200); ?>
+    <h1>Successfully sent message</h1>
+    <p><a href="http://mrkp.co">Click here</a> to go back to our site.</p> <?php
+
+  } else {
+
+    header("Status: 404 Not Found", true, 404); ?>
+    <h1>Error</h1>
+    <p>Oh, noes!</p> <?php
+
+  }
+
+?>
